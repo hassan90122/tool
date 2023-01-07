49ef4be1179331d72cc047aa6aab54eb19afbcc1
hassan90122 <70857287+hassan90122@users.noreply.github.com>
2023-01-07 04:13:46 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -2,48 +2,34 @@
 #include <stdlib.h>
 
 int main()
-
 {
-   int a,b;
-  char op;
-  printf("Enter Operator(+,-,*,/)");
-  scanf("%c",&op);
-printf("first  number:");
-  scanf("%d",&a);
+//Operations:
 
-  printf("second  number:");
-  scanf("%d",&b);
+//    int x =5;
+//    int y =3;
+//
+//    printf("The Sum is : %d", x+y);
+//    printf("The Difference is : %d", x-y);
+//    printf("The Multiply is : %d", x*y);
+//    printf("The Division is : %d", x/y);
+//    return 0;
 
-
-
-  /**switch(op)
-
-  {
-  case '+' : printf("Result : %d ",a+b);break;
-  case '-' : printf("Result : %d ",a-b);break;
-  case '*' : printf("Result : %d ",a*b);break;
-  case '/' : printf("Result : %d ",a/b);break;
-  default  : printf("Invalid operator....");
-
-
-}*/
-
-//another way:
-
-if(op=='+'){
-        printf("Result : %d ",a+b);
+//Check :
+int x;
+scanf("%d",&x);
+if(x>=0&&x<=10){
+    printf("the number between 0 and 10");
 }
-if(op=='-'){
-        printf("Result : %d ",a-b);
+else if (x>10 && x<=100){
+     printf("the number between 10 and 100");
 }
-if(op=='*'){
-        printf("Result : %d ",a*b);
+else if (x>100&&x<=500){
+     printf("the number between 100 and 500");
 }
-if(op=='/'){
-        printf("Result : %d ",a/b);
-
+else if (x>500&&x<=1000){
+     printf("the number between 500 and 1000");
 }
-
-
-
+else{
+    printf("ERROR");
+}
 }
